0bc0d5771fb87d0b7e88ea133b91742215521d89
Tom Tate <tomtate101@yahoo.com>
2026-02-03 18:03:33 -0800

    add fetchWeather(location) to fetch from Weather API using location from search bar. add getCurrentWeather(weather) to return necessary data from current day and getWeekWeather(weather) to return array containing 7-day weather data.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,56 @@
 // index.js
 import "./styles.css";
+import "./reset.css";
+
+const locationForm = document.querySelector(".location-form");
+locationForm.addEventListener("submit", (e)=> {
+  e.preventDefault();
+  
+  let location = locationForm.querySelector("#location").value;
+  console.log(location);
+  locationForm.reset();
+
+  fetchWeather(location).then(weather => {
+    console.log(getCurrentWeather(weather));
+    console.log(getWeekWeather(weather));
+  }).catch(err => {
+    console.error(err);
+  });
+  
+})
+
+async function fetchWeather(location){
+  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=K3G2LWP2CFRKQBQBG9G32U75K`
+  const response = await fetch(url);
+  if (!response.ok){
+    throw new Error(`HTTP error! Status: ${response.status}`);
+  }
+
+  const weatherJSON = await response.json();
+  return weatherJSON;
+}
+
+function getCurrentWeather(weather){
+  const currentWeather = weather.currentConditions;
+
+  return {
+    temp: currentWeather.temp,
+    conditions: currentWeather.conditions,
+    humidity: currentWeather.humidity,
+    precip: currentWeather.precip,
+    windspeed: currentWeather.windspeed,
+    uvIndex: currentWeather.uvindex,
+    sunset: currentWeather.sunset,
+    sunrise: currentWeather.sunrise,
+  }
+}
+
+function getWeekWeather(weather) {
+  let weekData = [];
+  for (let i = 0; i < 7; i++) {
+    let dayData = weather.days[i];
+    weekData[i] = {temp: dayData.temp, conditions: dayData.conditions};
+  }
+
+  return weekData;
+}
